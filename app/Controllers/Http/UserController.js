514366cc172e4fe59670ee1308c2bd4e8c9b1d11
Jay-Ayres <jayce.ayres@gmail.com>
2019-08-13 22:11:25 -0400

    ajuste controle

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -10,7 +10,7 @@ class UserController {
   }
 
   async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+    const data = request.only(['username', 'email', 'password', 'mandate', 'technologies', 'description'])
 
     const user = await User.create(data)
     return user
