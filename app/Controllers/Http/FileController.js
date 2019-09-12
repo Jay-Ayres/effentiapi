4261637d95d15136e358b1c8e5390e67ff8a7067
Jay-Ayres <jayce.ayres@gmail.com>
2019-09-11 21:37:39 -0400

    test route

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -49,7 +49,58 @@ class FileController {
     }).process()
   }
 
+
+
   async storePost ({ request, response, params }) {
+    console.log('log')
+    request.multipart.file('file', {}, async file => {
+      console.log("dentro do metodo")
+      /*
+      try {
+        const ContentType = file.headers['content-type']
+        const ACL = "public-read"
+        const key = `${Date.now()}.${file.subtype}`
+
+        const url = await Drive.put(key, file.stream, {
+          ContentType,
+          ACL
+        })
+
+        const imagem = await File.create({
+          name: file.clientName,
+          key: key,
+          url: url,
+          contentType: ContentType
+        })
+
+        console.log('imagem criada')
+
+        const data = { file_id: '' }
+        console.log('visualizando data')
+        console.log(data)
+
+        data.file_id = imagem.id
+
+        console.log('dtacom imagem')
+        console.log(data)
+
+        const post = await Post.findOrFail(params.id)
+        console.log('visualizando Post encontrado')
+        console.log(post)
+        post.merge(data)
+        console.log('Visualizando post apos merge')
+
+        await post.save()
+        console.log('post salvo')
+
+      } catch (error) {
+        return response.status(error.status).send({ error: { message: 'Erro ao fazer upload de arquivo' } })
+      }
+      */
+    }).process()
+  }
+
+  async storeOnly ({ request, response, params }) {
     console.log('log')
     request.multipart.file('file', {}, async file => {
       console.log("dentro do metodo")
