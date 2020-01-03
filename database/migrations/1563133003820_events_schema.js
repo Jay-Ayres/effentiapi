909c2aae3b867af921fbb45b32714f22f771aa82
jayce ayres <jayceayres@jayces-MacBook-Pro.local>
2020-01-03 18:36:13 -0500

    ajust event date

--- database/migrations/1563133003820_events_schema.js
+++ database/migrations/1563133003820_events_schema.js
@@ -1,27 +1,39 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class EventsSchema extends Schema {
-  up () {
-    this.create('events', (table) => {
-      table.increments()
-      table.integer('user_id').unsigned().references('id').inTable('users').onUpdate('CASCADE').onDelete('SET NULL')
-      table.integer('file_id').unsigned().references('id').inTable('files').onUpdate('CASCADE').onDelete('SET NULL')
-      table.string('name', 80).notNullable()
-      table.string('url_image', 300)
-      table.string('description', 100).notNullable()
-      table.date('limit_date')
-      table.date('event_date')
-      table.date('event_end_date')
-      table.timestamps()
-    })
+  up() {
+    this.create("events", table => {
+      table.increments();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("SET NULL");
+      table
+        .integer("file_id")
+        .unsigned()
+        .references("id")
+        .inTable("files")
+        .onUpdate("CASCADE")
+        .onDelete("SET NULL");
+      table.string("name", 80).notNullable();
+      table.string("url_image", 300);
+      table.string("description", 100).notNullable();
+      table.datetime("limit_date");
+      table.datetime("event_date");
+      table.datetime("event_end_date");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('events')
+  down() {
+    this.drop("events");
   }
 }
 
-module.exports = EventsSchema
+module.exports = EventsSchema;
